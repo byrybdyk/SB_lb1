36b8c86c3de02df17081e8ef2ef9767e3649b714
byrybdyk <90798897+byrybdyk@users.noreply.github.com>
2025-11-18 21:37:09 +0300

    fix ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.spotbugs") version "5.2.3"
 }
 
 group = "byrybdyk.me"
@@ -24,16 +25,36 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     compileOnly("org.projectlombok:lombok")
-    runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-    implementation("com.github.spotbugs")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+spotbugs {
+    toolVersion = "4.8.3"
+    effort = com.github.spotbugs.snom.Effort.MAX
+    reportLevel = com.github.spotbugs.snom.Confidence.LOW
+}
+
+tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
+    reports.create("html") {
+        required.set(true)
+        outputLocation.set(file("$buildDir/reports/spotbugs/${name}.html"))
+    }
+    reports.create("xml") {
+        required.set(true)
+        outputLocation.set(file("$buildDir/reports/spotbugs/${name}.xml"))
+    }
 }
